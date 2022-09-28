4dfa4227400b4a208208d6b5b8e303d6a4fa3282
wshelt21 <william.shelton24@gmail.com>
2022-09-28 14:14:59 -0400

    create game func and player score feature

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let choice = ["rock", "paper", "scissors"]
     let random = choice[Math.floor(Math.random() * choice.length)]
     return random;
 }
 
-
-let computerSelection = getComputerChoice();
-let playerSelection = prompt('Enter in Rock, Paper, or Scissors').toLowerCase();
-
 function playRound(playerSelection,computerSelection){
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    if(computerSelection === "paper" && playerSelection === 'rock'){
-    return  "You Lose. Paper beats Rock.";
 
+    if(computerSelection === "paper" && playerSelection === 'rock'){
+    computerScore++
+    return  "You Lose. Paper beats Rock.";
+    
 }else if(computerSelection === "paper" && playerSelection === 'scissors') {
+    playerScore++
     return "You Win! Scissors beat Paper";
 
 } else if(computerSelection === "scissors" && playerSelection === 'rock') {
+    playerScore++
     return "You Win! Rock beats Scissors.";
 
 }else if(computerSelection === "scissors" && playerSelection === 'paper') {
+    computerScore++
     return "You Lose. Scissors beat Paper.";
 
 }else if(computerSelection === "rock" && playerSelection === 'paper') {
+    playerScore++
     return "You Win! Paper beats Rock ";
 
 }else if(computerSelection === "rock" && playerSelection === 'scissors') {
+    computerScore++
     return "You Lose. Rock beat Scissors";
 
 } else {
@@ -36,5 +39,24 @@ function playRound(playerSelection,computerSelection){
 
 }
 
-console.log(playRound(playerSelection,computerSelection));
 
+
+/*console.log(playRound(playerSelection,computerSelection));*/
+
+function game(){
+    for(let i = 0; i <5; i++) {
+        let playerSelection = prompt('Enter in Rock, Paper, or Scissors').toLowerCase();
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection,computerSelection);
+}
+
+if (playerScore > computerScore){
+    return "Congrats! You Win!"
+} else if (playerScore < computerScore){
+    return "You Lose.";
+} else {
+    return "It's a Tie.";
+}
+}
+
+console.log(game());
